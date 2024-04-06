2a5cdef580d3ab903e459b3321e6eb08768f5ede
cyber Ahn <35652262+cyber-Ahn@users.noreply.github.com>
2024-04-06 20:12:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "\n\e[33m>> \e[31mEnter Your Python Version '3.11'?\e[32m"
 read ver
-sudo mv /home/pi/gpio_pi4_to_pi5_bridge/TGNgpio.py /usr/local/lib/python"$ver"/dist-packages/
+sudo mv /home/pi/Raspberry-PI-5-gpio-pi4_to_pi5_bridge/TGNgpio.py /usr/local/lib/python"$ver"/dist-packages/
 echo -e "\n\e[33m>> \e[31minstallation successful\e[32m"
 sleep 3
